6b583669ea97fd220e6a7b36579d6f8b5e89716b
libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-04-12 19:37:28 +0700

    Update PaginationScope.php

--- app/Traits/PaginationScope.php
+++ app/Traits/PaginationScope.php
@@ -4,10 +4,17 @@ namespace App\Traits;
 
 trait PaginationScope
 {
-	public function scopePagination($query, $page = 1, $pageSize = 10)
+	public function scopePagination($query, $page = 1, $pageSize = 10,  $sortDirection = 'desc', $sortBy = 'created_at')
 	{
 		$page = request()->get('page', $page);
 		$pageSize = request()->get('page_size', $pageSize);
-		return $query->latest()->skip(($page - 1) * $pageSize)->limit($pageSize)->get();
+		$sortDirection = request()->get('sort_direction', $sortDirection);
+		$sortBy = request()->get('sort_by', $sortBy);
+
+		if ($sortDirection != 'asc') {
+			$sortDirection = 'desc';
+		}
+
+		return $query->orderBy($sortBy, $sortDirection)->skip(($page - 1) * $pageSize)->limit($pageSize)->get();
 	}
 }
