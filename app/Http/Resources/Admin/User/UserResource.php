8e9c8ed1e7ddd0a777e2eaa94b8a4dde1e6f0bef
libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-04-21 23:45:18 +0700

    update

--- app/Http/Resources/Admin/User/UserResource.php
+++ app/Http/Resources/Admin/User/UserResource.php
@@ -6,24 +6,26 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return [
-            'id' => $this->id,
-            'first_name' => $this->first_name,
-            'last_name' => $this->last_name,
-            'user_name' => $this->user_name,
-            'avatar_url' => $this->avatar_url,
-            'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-        ];
-    }
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+	 */
+	public function toArray($request)
+	{
+		return [
+			'id' => $this->id,
+			'first_name' => $this->first_name,
+			'last_name' => $this->last_name,
+			'user_name' => $this->user_name,
+			'avatar_url' => $this->avatar_url,
+			'email' => $this->email,
+			'email_verified_at' => $this->email_verified_at,
+			'role' => $this->role,
+			'status' => $this->status,
+			'created_at' => $this->created_at,
+			'updated_at' => $this->updated_at,
+		];
+	}
 }
